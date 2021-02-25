86477d89caefc1250c03c755b04068c1e1f66b36
bronson <bronson.schultz1@gmail.com>
2021-02-25 17:23:01 -0600

    dealing with whitespace

--- 436_client/Client.py
+++ 436_client/Client.py
@@ -2,22 +2,32 @@ import socket
 import sys
 
 
+# create our connection to the server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# ip address and port number are received through the command line arguments
 host = str(sys.argv[1])
 port = int(sys.argv[2])
 
+# connect to the server through those arguments
 print("connecting to", host, "on port:", port, "...")
 server.connect((host, port))
 print('connected')
 
 
-while True:
 
+# while connected..
+while True:
+    # get any messages from the server
     data = server.recv(1024).decode()
+    if not data:
+        continue
+
     print(data)
 
-    message = sys.stdin.readline().encode('utf8')
-    server.send(message)
+    # send message to the server
+    message = sys.stdin.readline()
+    server.send(message.encode('utf8'))
 
 
 
--- 436_server/src/ClientThread.java
+++ 436_server/src/ClientThread.java
@@ -18,6 +18,7 @@ public class ClientThread extends Thread{
     public ClientThread(Server server, Socket socket){
         this.server = server;
         this.socket = socket;
+        currentRoom = "";
     }
 
     protected void sendMsgToClient(String message){
@@ -25,26 +26,21 @@ public class ClientThread extends Thread{
         writeToClient.flush();
     }
 
-    protected Boolean isValidCommand(String command){
-        List<String> validCommands = Arrays.asList("/help", "/list", "/create", "/join");
+    protected Boolean isCommand(String command){
+        return command.charAt(0) == '/';
+    }
 
-        if (command.charAt(0) == '/') {
-            if (validCommands.contains(command)){
-                return true;
-            }
-            else {
-                sendMsgToClient("Not a valid command, type '/help' to see the available commands!");
-                return false;
-            }
-        } else {
-            return false;
-        }
+    protected Boolean isValidCommand(String command){
+        List<String> validCommands = Arrays.asList("/help", "/list", "/create", "/join", "/leave", "/exit");
+
+
+        return validCommands.contains(command);
     }
 
     protected void executeCommand(String command){
         if(isValidCommand(command)){
-            switch (command){
-                case "/help": {
+            switch (command) {
+                case "/help" -> {
                     sendMsgToClient("""
                             /help --\s
                             /help - this list\s
@@ -54,48 +50,87 @@ public class ClientThread extends Thread{
                             /leave <name> - leave a joined chatroom of the given name\s
                             /exit - exit the chatroom program completely""");
                 }
-                case "/list": {
-                    //TODO
-
+                case "/list" -> {
+                    sendMsgToClient(list());
                 }
-                case "/create": {
+                case "/create" -> {
                     sendMsgToClient("Name of the new room?: ");
                     try {
                         createRoom(readFromClient.readLine());
-                    } catch (IOException e){
+                    } catch (IOException e) {
                         System.out.println(e.getMessage());
                     }
                 }
-
-                case "/join": {
+                case "/join" -> {
                     sendMsgToClient("which room?: ");
                     try {
-                        currentRoom = readFromClient.readLine();
-                    } catch (IOException e){
+                        String room = readFromClient.readLine();
+                        joinChatroom(room);
+                        sendMsgToClient(getMsgsFromRoom(room));
+                    } catch (IOException e) {
                         System.out.println(e.getMessage());
                     }
                 }
-                case "leave": {
+                case "/leave" -> {
+                    leave();
+                }
+                case "/exit" -> {
+                    try {
+                        sendMsgToClient("Goodbye!");
+                        System.out.println(socket.getRemoteSocketAddress() + " left ");
+                        socket.close();
 
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
                 }
             }
+        } else {
+            sendMsgToClient("That is not a valid command! type /help");
         }
     }
 
-    protected void createRoom(String name){
-        server.getChatrooms().put(name,"");
+    protected synchronized void createRoom(String name) {
+        if (server.getChatrooms().containsKey(name)) {
+            sendMsgToClient("The chatroom already exists!");
+        } else {
+            server.getChatrooms().put(name, "");
+            sendMsgToClient("created new room: " + name);
+        }
     }
 
-    protected void saveMsgToRoom(String roomName, String message){
+    protected synchronized void saveMsgToRoom(String roomName, String message){
         String roomHistory = server.getChatrooms().get(roomName);
         roomHistory = roomHistory + message + "\n";
         server.getChatrooms().put(roomName, roomHistory);
     }
 
-    protected void joinChatroom(String name){
-
+    protected String getMsgsFromRoom(String roomName){
+        if (server.getChatrooms().containsKey(roomName)){
+            return server.getChatrooms().get(roomName);
+        } else {
+            sendMsgToClient("that chatroom doesn't exist!");
+            return "";
+        }
+    }
+    protected String list(){
+        return server.getChatrooms().keySet().toString();
     }
 
+    protected void joinChatroom(String name){
+        if (server.getChatrooms().containsKey(name)) {
+            currentRoom = name;
+            sendMsgToClient("Joined room: "+ name);
+        } else {
+            sendMsgToClient("That chatroom doesn't exist!");
+        }
+    }
+
+    protected void leave(){
+        sendMsgToClient("Left room: " + currentRoom);
+
+        currentRoom = "";
+    }
     protected void loginUser() {
         sendMsgToClient("Please enter your name: ");
         try {
@@ -106,6 +141,19 @@ public class ClientThread extends Thread{
         }
     }
 
+    protected String getMessagefromUser(){
+        List<String> whitespace = Arrays.asList("", "\n", " ");
+        String message = "";
+        do {
+            try {
+                message = readFromClient.readLine();
+            } catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+        } while (whitespace.contains(message));
+        return message;
+    }
+
     @Override
     public void run(){
         try{
@@ -114,12 +162,26 @@ public class ClientThread extends Thread{
 
             loginUser();
             String msgFromClient;
-           do {
+            List<String> whitespace = Arrays.asList("", "\n", " ");
+            do {
                 msgFromClient = readFromClient.readLine();
-                executeCommand(msgFromClient);
+
+                if(whitespace.contains(msgFromClient)){
+                    continue;
+                }
 
 
-                //System.out.println(msgFromClient);
+                if (isCommand(msgFromClient)) {
+                    executeCommand(msgFromClient);
+                } else {
+                    if(currentRoom.equals("")){
+                        sendMsgToClient("You are not currently in a room!");
+                        continue;
+                    }
+                    sendMsgToClient("");
+                    saveMsgToRoom(currentRoom, msgFromClient);
+                    sendMsgToClient(getMsgsFromRoom(currentRoom));
+                }
             } while (!msgFromClient.equals("/exit"));
 
         } catch (IOException e){
